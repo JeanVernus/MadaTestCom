38b6ef7a969dfa24436c520ea077a19eb77434f9
JeanVernus <jeannywcs@gmail.com>
2020-05-12 11:18:23 +0200

    firstCommit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Accueil from './Components/Accueil'
+import './CSS/App.css';
+
+class App extends Component {
+  render (){
+   return(    
+   <div> 
+      <Accueil/>
+    </div>
+   )
+  }
+}
+
+export default App;
